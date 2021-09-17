2218ca2632ed918997c59aa11359ad9d4fb655f1
KristinaNa <nosik94@list.ru>
2021-09-17 16:25:49 +0300

    more plugs

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,10 @@
 defmodule DiscussWeb.TopicController do
     use DiscussWeb, :controller
     alias Discuss.Topic
-  
+
+    plug DiscussWeb.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+    plug :check_topic_owner when action in [:update, :edit, :delete]
+
     def index(conn, _params) do
         topics = Discuss.Repo.all(Topic)
 
@@ -15,7 +18,11 @@ defmodule DiscussWeb.TopicController do
     end
 
     def create(conn, %{"topic" => topic}) do
-        changeset = Topic.changeset(%Topic{}, topic)
+        # changeset = Topic.changeset(%Topic{}, topic)
+
+        changeset = conn.assigns.user
+            |> build_assoc(:topics)
+            |> Topic.changeset(topic)
 
         case Discuss.Repo.insert(changeset) do
             {:ok, _topic} ->
@@ -28,7 +35,7 @@ defmodule DiscussWeb.TopicController do
         end
     end
 
-    def edit(conn, %{"id" => topic_id }) do        
+    def edit(conn, %{"id" => topic_id }) do
         topic = Discuss.Repo.get(Topic, topic_id)
         changeset = Topic.changeset(topic)
 
@@ -40,23 +47,35 @@ defmodule DiscussWeb.TopicController do
         changeset = Topic.changeset(old_topic, topic)
 
         case Discuss.Repo.update(changeset) do
-            {:ok, _topic} -> 
-                conn                
+            {:ok, _topic} ->
+                conn
                 |> put_flash(:info, "Topic updated")
                 |> redirect(to: Routes.topic_path(conn, :index))
-            {:error, changeset} -> 
+            {:error, changeset} ->
                 render conn, "edit.html", changeset: changeset, topic: old_topic
         end
     end
 
     def delete(conn, %{"id" => topic_id}) do
         Discuss.Repo.get!(Topic, topic_id) |> Discuss.Repo.delete!
-    
-        conn 
+
+        conn
         |> put_flash(:info, "Topic deleted")
         |> redirect(to: Routes.topic_path(conn, :index))
 
         # render conn, "index.html", changeset: changeset
     end
+
+    def check_topic_owner(conn, _params) do
+        %{params: %{"id" => topic_id}} = conn
+
+        if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+            conn
+        else
+            conn
+            |> put_flash(:error, "you cannot edit that")
+            |> redirect(to: topic_path(conn, :index))
+            |> halt()
+        end
+    end
   end
-  
